6e253119fb39fe7776da3e17c9b723ffdaf9754e
Mariane Rodrigues <marianectrodrigues@gmail.com>
2024-11-18 19:46:21 -0300

    melhora do desafio de massa corporal - mundo 2

--- mundo2/indice_massa_corporal.py
+++ mundo2/indice_massa_corporal.py
@@ -1,29 +1,24 @@
-'''Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
-
+"""Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
 - IMC abaixo de 18,5: Abaixo do Peso
 - Entre 18,5 e 25: Peso Ideal
 - 25 até 30: Sobrepeso
 - 30 até 40: Obesidade
-- Acima de 40: Obesidade Mórbida'''
+- Acima de 40: Obesidade Mórbida"""
+# Desafio estendido com adição das outras categorias existentes atualmente
 
-altura = float(input('Digite a altura da pessoa (m): '))
-peso = float(input('Digite o peso da pessoa (kg): '))
-
+peso = float(input('Qual o peso da pessoa? '))
+altura = float(input('Qual a altura da pessoa? '))
 imc = peso / (altura ** 2)
-
-print(f'O IMC da pessoa é {imc:.1f}.')
-
-if imc < 18.5:
-    print('A pessoa está abaixo do peso ideal.')
-
-elif imc >= 18.5 and imc <= 25:
+print(f'O IMC é de {imc:.2f}.')
+if imc <= 18.5:
+    print('A pessoa está abaixo do peso.')
+elif 18.5 < imc < 25:
     print('A pessoa está no peso ideal.')
-
-elif imc > 25 and imc <= 30:
+elif 25 <= imc < 30:
     print('A pessoa está com sobrepeso.')
-
-elif imc > 30 and imc <= 40:
-    print('A pessoa está com obesidade.')
-
+elif 30 <= imc < 35:
+    print('A pessoa está com obesidade grau I.')
+elif 35 <= imc < 40:
+    print('A pessoa está com obesidade grau II.')
 else:
-    print('A pessoa está com obesidade mórbida.')
+    print('A pessoa está com obesidade grau III.')
